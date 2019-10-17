a5b737a768b2b09c9e1638975a8a50835014b6fb
David Andrew Towers <d.towers2@ncl.ac.uk>
2019-10-17 10:26:38 +0100

    Added a Brute force function to test more efficient functions against

--- CSC8501-Coursework/CSC8501-Coursework.cpp
+++ CSC8501-Coursework/CSC8501-Coursework.cpp
@@ -1,20 +1,80 @@
-// CSC8501-Coursework.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int factorial(int n) {
+	if (n == 1)
+		return 1;
+	return n * factorial(n - 1);
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int findConRows(int ind, int* arr, int size) {
+	for (int i = 0; i < size; i++) {
+		if (arr[ind] == arr[i] - 1) {
+			for (int j = 0; j < size; j++) {
+				if (arr[i] == arr[j] - 1)
+					return (factorial(size - 2)) + (factorial(size - 3) / 2);
+			}
+			return (factorial(size - 2) / 2);
+		}
+	}
+	return 0;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int bruteforce(int* board) {
+	int rows = 0;
+	for (int a = 0; a < 8; a++) {
+		for (int b = 0; b < 8; b++) {
+			if (b != a) {
+				for (int c = 0; c < 8; c++) {
+					if (c != a && c != b) {
+						for (int d = 0; d < 8; d++) {
+							if (d != a && d != b && d != c) {
+								for (int e = 0; e < 8; e++) {
+									if (e != a && e != b && e != c && e != d) {
+										for (int f = 0; f < 8; f++) {
+											if (f != a && f != b && f != c && f != d && f != e) {
+												for (int g = 0; g < 8; g++) {
+													if (g != a && g != b && g != c && g != d && g != e && g != f) {
+														for (int h = 0; h < 8; h++) {
+															if (h != a && h != b && h != c && h != d && h != e && h != f && h != g) {
+																if (board[a] == board[b] - 1 && board[b] == board[c] - 1) {
+																	rows++;
+																} else if (board[d] == board[e] - 1 && board[e] == board[f] - 1) {
+																	rows++;
+																} else if (board[g] == board[h] - 1) {
+																	rows++;
+																}
+															}
+														}
+													}
+												}
+											}
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+	return rows / 2;
+}
+
+int main() {
+	int array_size = 8;
+	int* board = new int[array_size] { 1, 18, 6, 7, 8, 9, 16, 13};
+	int rowNum = 0;
+
+	for (int i = 0; i < array_size; i++) {
+		rowNum += findConRows(i, board, array_size);
+	}
+
+	;
+	cout << rowNum << endl;
+	cout << bruteforce(board) << endl;
+}
+
--- README.md
+++
@@ -1,2 +0,0 @@
-# CSC8501-Coursework
-Repo to contain Coursework 1 &amp; 2 of CSC8501 - Advanced Programming for Games
