0edf6a13013e4ce33478599f89a83266fa3d042d
David Andrew Towers <d.towers2@ncl.ac.uk>
2019-10-22 11:15:53 +0100

    Program will now prompt to restart

--- CSC8501-Coursework/CSC8501-Coursework.cpp
+++ CSC8501-Coursework/CSC8501-Coursework.cpp
@@ -7,6 +7,9 @@
 
 using namespace std;
 
+const string CON_FILE = "15-File";
+const string RES_FILE = "Solution-File";
+
 //Functions used by class, but not apart of object
 int factoriall(int n) {
 	if (n == 1)
@@ -17,20 +20,6 @@ int factoriall(int n) {
 //vector<int>* board = new vector<int>{ 1, 18, 6, 7, 8, 9, 16, 13 };
 //vector<int>* board = new vector<int>{ 1, 2, 3, 4, 5, 6, 7, 8, };
 //vector<int>* board = new vector<int>{ 4, 19, 12, 6, 18, 3, 17, 10, 2, 9, 16, 5, 15, 11, 7 };
-//cout << "Old Method: " << Test(board) << endl;
-//int Test(vector<int>* board) {
-//	int trips = 0;
-//	int dubs = 0;
-//	for (int i = 0; i < board->size() - 1; i++) {
-//		if (board->at(i) == board->at(i + 1) - 1) {
-//			dubs++;
-//			if (i != board->size() - 2 && board->at(i) == board->at(i + 2) - 2)
-//				trips++;
-//		}
-//	}
-//
-//	return (dubs * factoriall(8 - 2)) / 2 + (trips * factoriall(8 - 3));
-//}
 
 vector<int> manual() {
 	cout << "Please Enter in 15 Numbers (1-20) to make up your configuration" << endl;
@@ -55,7 +44,6 @@ vector<int> manual() {
 }
 
 void saveConfigurations(vector<Puzzle> vec) {
-	const string CON_FILE = "15-File";
 	ofstream output(CON_FILE.c_str());
 	output << vec.size() << endl;
 	for (Puzzle p : vec)
@@ -64,7 +52,6 @@ void saveConfigurations(vector<Puzzle> vec) {
 }
 
 void saveResults(vector <Puzzle> vec) {
-	const string RES_FILE = "Solution-File";
 	ofstream output(RES_FILE.c_str());
 	output << vec.size() << endl;
 	for (Puzzle p : vec)
@@ -73,7 +60,6 @@ void saveResults(vector <Puzzle> vec) {
 }
 
 vector<Puzzle> loadConfigurations(vector<Puzzle> vec) {
-	const string CON_FILE = "15-File";
 	ifstream input(CON_FILE.c_str());
 	vector<int> temp;
 	int buff;
@@ -91,38 +77,47 @@ vector<Puzzle> loadConfigurations(vector<Puzzle> vec) {
 }
 
 int main() {
-	vector<Puzzle> vec;
-	srand(time(0));
-	char choice;
-	int num = 1;
-	cout << "Would you like to manually (m) enter a puzzle or randomly (r) generate Puzzles or load (l) from file?: ";
-	cin >> choice;
-	switch (choice){
-		case ('m'):
-			vec.push_back(Puzzle(manual()));
-			break;
-		case ('r'):
-			cout << "How many puzzles would you like to generate?: ";
-			cin >> num;
-			for (int i = 0; i < num; i++) {
-				vec.push_back(Puzzle(4));
-				vec.at(i).genPuzzle();
-			}
+	bool repeat = true;
+	while (repeat) {
+		vector<Puzzle> vec;
+		srand(time(0));
+		char choice;
+		int num = 1;
+		cout << "Would you like to manually (m) enter a puzzle or randomly (r) generate Puzzles or load (l) from file?: ";
+		cin >> choice;
+		switch (choice) {
+			case ('m'):
+				vec.push_back(Puzzle(manual()));
+				break;
+			case ('r'):
+				cout << "How many puzzles would you like to generate?: ";
+				cin >> num;
+				for (int i = 0; i < num; i++) {
+					vec.push_back(Puzzle(4));
+					vec.at(i).genPuzzle();
+				}
 
-			break;
-		case ('l'):
-			vec = loadConfigurations(vec);
-			break;
+				break;
+			case ('l'):
+				vec = loadConfigurations(vec);
+				break;
+		}
+
+		cout << "Saving Configuration" << endl;
+		saveConfigurations(vec);
+
+		cout << vec.size() << endl;
+		for (Puzzle p : vec) {
+			cout << p.resultString() << endl;
+		}
+
+		cout << "Saving Results" << endl;
+		saveResults(vec);
+
+		cout << "Would you like to restart the program? (Y or N): ";
+		cin >> choice;
+
+		if (choice == 'n')
+			repeat = false;
 	}
-
-	cout << "Saving Configuration" << endl;
-	saveConfigurations(vec);
-
-	cout << vec.size() << endl;
-	for (Puzzle p : vec) {
-		cout << p.resultString() << endl;
-	}
-
-	cout << "Saving Results" << endl;
-	saveResults(vec);
 }
